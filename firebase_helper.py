396900f1f94f6c1ef2aacbdd4341c4d4df8ea832
Manikandan1323 <manikandancse2004@gmail.com>
2025-06-27 22:32:30 +0530

    Add files via upload
    Currently on final stage

---
+++ firebase_helper.py
@@ -0,0 +1,16 @@
+# firebase_helper.py
+import pyrebase
+import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def init_firebase():
+    firebase_config_path = os.getenv("FIREBASE_CONFIG") or "firebase_config.json"
+
+    with open(firebase_config_path) as f:
+        config = json.load(f)
+
+    firebase = pyrebase.initialize_app(config)
+    return firebase
