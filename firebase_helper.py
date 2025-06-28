f17b133e73699a681bda41e299b1468400d3fcf2
Manikandan1323 <manikandancse2004@gmail.com>
2025-06-28 20:24:03 +0530

    Update firebase_helper.py

--- firebase_helper.py
+++ firebase_helper.py
@@ -1,18 +1,16 @@
 # firebase_helper.py
-import pyrebase
 import streamlit as st
-from dotenv import load_dotenv
+import pyrebase
 import firebase_admin
 from firebase_admin import credentials, firestore
-import streamlit as st
-
-
-load_dotenv()
 
 def init_firebase():
     if not firebase_admin._apps:
         service_account_info = dict(st.secrets["firebase_admin"])
         cred = credentials.Certificate(service_account_info)
         firebase_admin.initialize_app(cred)
-    
     return firestore.client()
+
+def init_pyrebase():
+    config = st.secrets["firebase"]
+    return pyrebase.initialize_app(config)
