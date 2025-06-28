dfd9434d939ef4fa1aa51bf2e16a6cc0806e2340
Manikandan1323 <manikandancse2004@gmail.com>
2025-06-28 19:36:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 init_gemini(GEMINI_API_KEY)
 
 if not firebase_admin._apps:
-    cred = credentials.Certificate("serviceAccountKey.json")
+    cred = credentials.Certificate(st.secrets["firebase_admin"])
     firebase_admin.initialize_app(cred)
 db = firestore.client()
 
