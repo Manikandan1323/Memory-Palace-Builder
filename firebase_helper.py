6bf22d0e7bf231a3844194e9c288b2a87c52e34e
Manikandan1323 <manikandancse2004@gmail.com>
2025-06-28 19:29:01 +0530

    Update firebase_helper.py

--- firebase_helper.py
+++ firebase_helper.py
@@ -1,16 +1,13 @@
 # firebase_helper.py
 import pyrebase
-import json
-import os
 import streamlit as st
 from dotenv import load_dotenv
 
 load_dotenv()
 
 def init_firebase():
-    config = st.secrets["firebase"])
-    return pyrebase.initialize_app(config)
-
+    config = st.secrets["firebase"]
     firebase = pyrebase.initialize_app(config)
     return firebase
+
 
